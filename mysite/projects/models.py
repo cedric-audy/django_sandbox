a1ec88abfc7217e4f1c5336725032affdec7a30b
Cédric Audy <cedric.audy08@gmail.com>
2020-07-07 18:32:59 -0400

    shows list of recent model objects

--- mysite/projects/models.py
+++ mysite/projects/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Project(models.Model):
-    text = 'test project'
+    text = models.CharField(max_length=200, default='none')
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.text
--- mysite/projects/views.py
+++ mysite/projects/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Project
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the project index.")
+    project_list = Project.objects.order_by('-pub_date')[:5]
+    context = {'project_list': project_list}
+    return render(request, 'projects/index.html', context)
