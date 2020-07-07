cad727f4278e99316dfc0356449da37d4d140868
Cédric Audy <cedric.audy08@gmail.com>
2020-07-07 18:56:37 -0400

    more details

--- mysite/projects/views.py
+++ mysite/projects/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from .models import Project
 
@@ -7,3 +8,10 @@ def index(request):
     project_list = Project.objects.order_by('-pub_date')[:5]
     context = {'project_list': project_list}
     return render(request, 'projects/index.html', context)
+
+def detail(request, project_id):
+    try:
+        project = Project.objects.get(pk=project_id)
+    except Project.DoesNotExist:
+        raise Http404("Project does not exist")
+    return render(request, 'projects/detail.html', {'project': project})
