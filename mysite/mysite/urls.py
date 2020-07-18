871a8dbfb6321fd6e7262ffcba11beedb18f8f52
Cédric Audy <cedric.audy08@gmail.com>
2020-07-17 21:37:11 -0400

    changed urls again

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('index.html'),
+    path('index.html', include('projects.urls')),
     path('projects/', include('projects.urls')),
     path('admin/', admin.site.urls)
 ]
