bd0af72991ce4318db3b4e76c817d6fa9157510c
cedric-audy <cedric.audy08@gmail.com>
2020-07-17 22:48:36 -0400

    msigh

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import TemplateView
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
    path('', views.index, name='index'),
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
+from . import views
 
 def index(request):
-    return render(request, 'index.html', context)
+    return render(request, '/index.html')
